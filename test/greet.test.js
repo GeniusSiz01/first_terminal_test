e6875d31c8ca2144dd57d613d4d79a921186d7a3
GeniusSiz01 <sizwe.msimango01@gmail.com>
2021-05-11 11:40:18 +0200

    my initial commit

--- test/greet.test.js
+++ test/greet.test.js
@@ -3,10 +3,10 @@ let greet = require("../greet");
 
 describe('The greet function', function(){
 
-    it('should greet Andrew correctly', function(){
+    it('should greet Sizwe correctly', function(){
         assert.equal('Hello, Sizwe', greet('Sizwe'));
     });
-    it('should greet Karen correctly', function(){
+    it('should greet Rennay correctly', function(){
         // this test will fail - can you fix it?
         assert.equal('Hello, Rennay', greet('Rennay'));
     });
